45fc9b158227df0326639c08f4b270a97e157ceb
Cheryl Hughey <cherylhughey@hotmail.com>
2019-02-13 20:17:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -89,13 +89,6 @@ function initMap() {
           });
 
           // Make markers clickable
-          layer.on('click', function() {
-            $("div#contents").animate({scrollTop: areaTop + "px"});
-          });
-
-        })(layer, feature.properties);
-      }
-    });
 
     $('div#container1').addClass("inFocus");
     $('#contents').append("<div class='space-at-the-bottom'><a href='#space-at-the-top'><i class='fa fa-chevron-up'></i></br><small>Top</small></a></div>");
